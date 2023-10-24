5115efc069bb0ecbcd8412a30773ad44003b26e2
hienpg <hienpg@ftech.ai>
2023-10-24 14:14:48 +0700

    update: header

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="header-containter">
-    this is header!
+  <div class="header-wrapper">
+    <div class="header-cont" >
+      <!-- * LOGO -->
+      <img src="../assets/image/logo/logo.png" alt="logo">
+      <!-- * items -->
+      <ul>
+        <li v-for="item in headItems" :key="item">{{ item }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      headItems: [
+        'product',
+        'brands',
+        'why triablo',
+        'team',
+        'reviews',
+        'about us',
+        'news',
+        'contacts',
+      ]
+    }
+  }
 };
 </script>
 
 <style lang="scss">
-.header-containter {
-  background: white;
+.header-wrapper {
+  background: black;
+  color: white;
+
+  .header-cont {
+    width: 1400px;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+
+    ul {
+      display: flex;
+      list-style: none;
+      li {
+        flex-shrink: 0;
+        padding: 10px;
+        margin: 10px;
+      }
+    }
+  }
 
 }
 </style>
