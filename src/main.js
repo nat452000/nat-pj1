8b80a237adcc0ecb257408e31e50d02d97e71942
hienpg <hienpg@ftech.ai>
2023-10-24 13:52:32 +0700

    fix: change package

--- src/main.js
+++ src/main.js
@@ -2,20 +2,10 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
-import Vue from "vue";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-
-// Import Bootstrap and BootstrapVue CSS files (order is important)
-// import "bootstrap/dist/css/bootstrap.css";
-// import "bootstrap-vue/dist/bootstrap-vue.css";
-// import "aos/dist/aos.scss";
 import "./app.scss";
-
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin);
+import Aos from "aos";
 
 const app = createApp(App);
 app.use(router);
+app.use(Aos)
 app.mount("#app");
